dc47df3235781a6e751f6e3279b48ed86f55b495
HowsonLiu <948470636@qq.com>
2019-08-04 17:19:50 +0800

    schedule function

--- run.py
+++ run.py
@@ -6,12 +6,25 @@
 
 import myemail.email_sender
 import forex.boc_spider
+import schedule
+import time
 
-emailsender=myemail.email_sender.EmailSender()
+def forex_run():
+    global emailsender
+    try:
+        spider = forex.boc_spider.BOCSpider()
+        spider.get_fc_detail()
+        spider.boc_history_handler.write_and_save_today(spider.today_fc_detail_list)
+        emailsender.send_forex('Today Forex', spider.fill_html())
+    except Exception as e:
+        emailsender.send_forex('Error', repr(e))
+
+
+emailsender = myemail.email_sender.EmailSender()
 emailsender.init_sender()
 emailsender.init_forex()
-spider = forex.boc_spider.BOCSpider()
-spider.get_fc_detail()
-spider.boc_history_handler.write_and_save_today(spider.today_fc_detail_list)
-emailsender.send_forex('Today Forex', spider.fill_html())
-
+forex_run()
+schedule.every().day.at('08:00').do(forex_run)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
