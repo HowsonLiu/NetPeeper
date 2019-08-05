e7402a94cff8ad4ff3454d13689e7b9c8238fd81
HowsonLiu <948470636@qq.com>
2019-08-05 10:40:42 +0800

    server object will create before login

--- myemail/email_sender.py
+++ myemail/email_sender.py
@@ -30,15 +30,15 @@ class EmailSender:
         self.sender_account = conf.get('email_sender', 'account')
         self.sender_password = conf.get('email_sender', 'password')
         self.sender_name = conf.get('email_sender', 'display_name')
-        self.sender = smtplib.SMTP_SSL(self.sender_address, self.sender_port)
 
     def init_forex(self):
         conf = configparser.ConfigParser()
         conf.read(CONFIG_PATH, encoding='utf-8')
-        for k,v in conf.items('forex_recv_email_account'):
+        for k, v in conf.items('forex_recv_email_account'):
             self.forex_recv_account.append(v)
 
     def send_forex(self, subject_text, html_text):
+        self.sender = smtplib.SMTP_SSL(self.sender_address, self.sender_port)
         self.sender.login(self.sender_account, self.sender_password)
         print('Note: Login success!')
         msg = MIMEText(html_text, 'html', 'utf-8')
